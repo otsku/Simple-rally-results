38ef8e8713c35919e5597589f0e28e376cbffd4d
uksto <77827470+uksto@users.noreply.github.com>
2022-05-22 23:08:52 +0300

    Reformat events list

--- app/src/main/java/fi/tuni/myapplication/MyAdapter.kt
+++ app/src/main/java/fi/tuni/myapplication/MyAdapter.kt
@@ -11,7 +11,8 @@ import android.widget.TextView
 class MyAdapter(context: Context, resource: Int, private val arrayList: ArrayList<Items>) : ArrayAdapter<Items>(context, resource, arrayList) {
     private lateinit var eventcount: TextView
     private lateinit var name: TextView
-    private lateinit var winner: TextView
+    private lateinit var status: TextView
+    private lateinit var country: TextView
 
     fun add(item: Items) {
         arrayList.add(item)
@@ -35,10 +36,12 @@ class MyAdapter(context: Context, resource: Int, private val arrayList: ArrayLis
         convertview = LayoutInflater.from(context).inflate(R.layout.item, parent, false)
         eventcount = convertview.findViewById(R.id.countTextView)
         name = convertview.findViewById(R.id.nameTextView)
-        winner = convertview.findViewById(R.id.winnerTextView)
+        status = convertview.findViewById(R.id.statusTextView)
+        country = convertview.findViewById(R.id.countryTextView)
         eventcount.text = (position + 1).toString()
         name.text = arrayList[position].name
-        winner.text = arrayList[position].winner?.lastName
+        country.text = "Country: " + arrayList[position].nation?.name
+        status.text = "Event status: " + arrayList[position].status?.name
         return convertview
     }
 }
