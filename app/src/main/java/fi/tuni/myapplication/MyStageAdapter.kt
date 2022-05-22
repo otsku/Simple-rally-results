43106cf9b8025ede4b67e05e3434d7c3e04d23c1
uksto <77827470+uksto@users.noreply.github.com>
2022-05-22 22:15:41 +0300

    Add list of results after stage

---
+++ app/src/main/java/fi/tuni/myapplication/MyStageAdapter.kt
@@ -0,0 +1,79 @@
+package fi.tuni.myapplication
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.ArrayList
+import java.util.concurrent.TimeUnit
+
+class MyStageAdapter(context: Context, resource: Int, private val arrayList: ArrayList<Result>, private val drivers: ArrayList<Entrant?>) : ArrayAdapter<Result>(context, resource, arrayList) {
+    private lateinit var eventcount: TextView
+    private lateinit var name: TextView
+    private lateinit var diffTime: TextView
+    private lateinit var totalTime: TextView
+    private lateinit var classView: TextView
+    private lateinit var manifacturer: TextView
+
+    fun add(item: Result, driver: Entrant?) {
+        arrayList.add(item)
+        drivers.add(driver)
+    }
+
+    fun getList(): ArrayList<Result> {
+        return arrayList
+    }
+
+    override fun getItem(position: Int): Result {
+        return arrayList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    @SuppressLint("ViewHolder")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var convertview = convertView
+        convertview = LayoutInflater.from(context).inflate(R.layout.stage_item, parent, false)
+        eventcount = convertview.findViewById(R.id.positionTextView)
+        name = convertview.findViewById(R.id.nameTextView)
+        diffTime = convertview.findViewById(R.id.diffTextView)
+        totalTime = convertview.findViewById(R.id.totalTextView)
+        classView = convertview.findViewById(R.id.classTextView)
+        manifacturer = convertview.findViewById(R.id.manifacturerTextView)
+        eventcount.text = arrayList[position].position.toString()
+        name.text = drivers[position]?.driver?.fullName.toString()
+        val tTime = getTime(arrayList[position].totalTimeMs!!)
+        totalTime.text = "Total time: $tTime s"
+        val dTime = getTime(arrayList[position].diffFirstMs!!)
+        diffTime.text = "Diff to first: +$dTime s"
+        classView.text = "Group: " + drivers[position]?.group?.name
+        manifacturer.text = "Manifacturer: "+ drivers[position]?.manufacturer?.name
+        return convertview
+    }
+
+    fun getTime(milliseconds: Int) : String {
+        val hours = milliseconds / 1000 / 60 / 60 % 24
+        var minutes = (milliseconds / 1000 / 60 % 60).toString()
+        if(minutes.length == 1) {
+            minutes = "0$minutes"
+        }
+        var seconds = (milliseconds / 1000 % 60).toString()
+        if(seconds.length == 1) {
+            seconds = "0$seconds"
+        }
+        val tenths = milliseconds / 100 % 10
+        if(hours == 0 && minutes == "00") {
+            return "$seconds.$tenths"
+        }
+        if(hours == 0) {
+            return "$minutes:$seconds.$tenths"
+        }
+        return "$hours:$minutes:$seconds.$tenths"
+    }
+}
---
+++ app/src/main/java/fi/tuni/myapplication/StageActivity.kt
@@ -0,0 +1,83 @@
+package fi.tuni.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.Serializable
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Result(var entryId: Int? = 0, var stageTimeMs: Int? = 0, var totalTimeMs: Int? = 0, var position: Int? = 0, var diffFirstMs: Int? = 0) : Serializable
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Results(var results: MutableList<Result>? = null) : Serializable
+
+class StageActivity() : AppCompatActivity() {
+    private lateinit var stage: Stage
+    private lateinit var listview: ListView
+    private lateinit var adapter: MyStageAdapter
+    private lateinit var entrants: Entrants
+    private var eventId: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stage)
+        stage = intent.getSerializableExtra("stage") as Stage
+        entrants = intent.getSerializableExtra("entrants") as Entrants
+        eventId = intent.getSerializableExtra("event_id") as Int
+        listview = findViewById<ListView>(R.id.listView)
+        adapter = MyStageAdapter(this, R.layout.item, ArrayList<Result>(), ArrayList<Entrant?>())
+        listview.adapter = adapter
+    }
+
+    override fun onStart() {
+        super.onStart()
+        thread() {
+            val mp = ObjectMapper()
+            var resultsraw = getUrl("https://api.wrc.com/results-api/rally-event/" + eventId + "/stage-result/stage-external/" + stage.stageId)
+            resultsraw = resultsraw?.dropLast(4)
+            resultsraw = "{\"results\":" + resultsraw.toString() + "}"
+            val results: Results = mp.readValue(resultsraw, Results::class.java)
+            if(adapter.getList().size == 0) {
+                results.results?.forEach {
+                    runOnUiThread() {
+                        var entrant: Entrant? = null
+                        for(i in entrants.entrants!!) {
+                            if(it.entryId == i.entryId) {
+                                entrant = i
+                            }
+                        }
+                        adapter.add(it, entrant)
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        }
+    }
+
+    fun getUrl(url: String) : String? {
+        var result: String? = null
+        val sb = StringBuffer()
+        val myUrl = URL(url)
+        val conn = myUrl.openConnection() as HttpURLConnection
+        val reader = BufferedReader(InputStreamReader(conn.getInputStream()))
+
+        reader.use {
+            var line: String? = null
+
+            do {
+                line = it.readLine()
+                sb.append(line)
+            } while(line !== null)
+            result = sb.toString()
+        }
+        return result
+    }
+}
