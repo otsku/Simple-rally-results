757c8809b37edf0879cd4aec3fc72411d6de1bc4
uksto <77827470+uksto@users.noreply.github.com>
2022-05-22 18:58:43 +0300

    Reformat code

--- app/src/main/java/fi/tuni/myapplication/MainActivity.kt
+++ app/src/main/java/fi/tuni/myapplication/MainActivity.kt
@@ -1,33 +1,34 @@
 package fi.tuni.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.io.Serializable
 import java.net.HttpURLConnection
 import java.net.URL
 import kotlin.concurrent.thread
 
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Nation(var id: Int? = 0, var name: String? = null, var alpha2: String? = null, var alpha3: String? = null, var ioc: String? = null)
+data class Nation(var id: Int? = 0, var name: String? = null, var alpha2: String? = null, var alpha3: String? = null, var ioc: String? = null) : Serializable
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Winner(var id: Int? = 0, var firstName: String? = null, var lastName: String? = null)
+data class Winner(var id: Int? = 0, var firstName: String? = null, var lastName: String? = null) : Serializable
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Items(var id: Int? = 0, var name: String? = null, var nation: Nation? = null, var winner: Winner? = null)
+data class Items(var id: Int? = 0, var name: String? = null, var nation: Nation? = null, var winner: Winner? = null) : Serializable
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class RallyEvents(var total : Int? = 0, var items: MutableList<Items>? = null)
+data class RallyEvents(var total : Int? = 0, var items: MutableList<Items>? = null) : Serializable
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class WrcJsonObject(var rallyEvents: RallyEvents? = null)
+data class WrcJsonObject(var rallyEvents: RallyEvents? = null) : Serializable
 
 class MainActivity : AppCompatActivity() {
     private lateinit var listview: ListView
@@ -44,11 +45,14 @@ class MainActivity : AppCompatActivity() {
         listview.adapter = adapter
 
         listview.setOnItemClickListener { parent, _, position, _ ->
-            val selectedItem = parent.getItemAtPosition(position)
-            println(selectedItem)
+            val selectedItem: Serializable = parent.getItemAtPosition(position) as Serializable
+            val intent = Intent(
+                this@MainActivity,
+                EventActivity::class.java
+            )
+            intent.putExtra("item", selectedItem);
+            startActivity(intent)
         }
-
-
     }
 
 
